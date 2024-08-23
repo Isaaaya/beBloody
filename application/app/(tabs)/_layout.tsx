f541d66f4c36f1b8d91a89be7d5bcced80b0bf21
Isaaaya <snizhanna.bushko@gmail.com>
2024-08-23 11:40:17 +0300

    Add tabs navbar

--- application/app/(tabs)/_layout.tsx
+++ application/app/(tabs)/_layout.tsx
@@ -1,31 +1,45 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {Image, ImageSourcePropType, View, Text} from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
-  const tabs = ['home', 'search', 'report', 'profile'];
+  const screenOptions = {
+    tabBarStyle:{
+      height: 65,
+      paddingTop: 20,
+    },
+    tabBarShowLabel: false,
+  };
+
+  const tabs = [
+    {caption: 'home', icon: require('../../assets/icons/home.png')}, 
+    {caption: 'search', icon: require('../../assets/icons/search.png')}, 
+    {caption: 'report', icon: require('../../assets/icons/report.png')}, 
+    {caption: 'profile', icon: require('../../assets/icons/name.png')}
+  ];
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        ...screenOptions,
         headerShown: false,
       }}>
-        {tabs.map((tab) => <Tabs.Screen key={tab}
-        name={tab}
+        {tabs.map((tab) => <Tabs.Screen key={tab.caption}
+        name={tab.caption}
         options={{
-          title: tab,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: tab.caption,
+          tabBarIcon: ({ focused }) => (
+            <View className="relative items-center">
+                <Image className="w-6 h-6" source={tab.icon as ImageSourcePropType} />
+                {focused && (
+                  <View className="absolute -bottom-3">
+                    <View className="w-2 h-2 bg-red-500 rounded-full" />
+                  </View>
+                )}
+              </View>
           ),
         }}
       />)}
-      
     </Tabs>
   );
 }
